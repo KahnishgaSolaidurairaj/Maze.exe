883da70137d60833f59ad1e673232468521a245f
KahnishgaSolaidurairaj <krishnakani19@gmail.com>
2026-03-14 14:14:22 -0500

    overall UI and design changes

--- Assets/Script/HeartPickup.cs
+++ Assets/Script/HeartPickup.cs
@@ -1,24 +1,19 @@
 using UnityEngine;
 
-public class HeartPickup : MonoBehaviour
-{
+public class HeartPickup : MonoBehaviour {
     public float rotateSpeed = 60f;
     public QuestionFlowManager qfm;
 
-    void Update()
-    {
+    void Update() {
         transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player") && qfm != null)
-        {
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player") && qfm != null) {
             Debug.Log("Heart collected!");
 
-            // Call the public method instead of accessing private variable
+            // Calls the public method instead of accessing private variable
             qfm.AddHeart();
-
             Destroy(gameObject);
         }
     }
